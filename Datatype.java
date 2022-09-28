3431e19635cf32616590530a259085befcd8eff1
Haja Mohideen <97336864+haja1221@users.noreply.github.com>
2022-09-28 09:35:47 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,24 @@
+public class Datatype{
+public static void main(String[]args){
+   String name= "haja";
+    int age=22;
+    float salary=100000;
+    double weight=75.5;
+    double height=175;
+    char fav='s';
+    long annualin=10000000l;
+    boolean select=false;
+   
+  System.out.println("name="+name+
+                    "\nage="+age+
+                    "\nsalary="+salary+
+                    "\nweight="+weight+
+                    "\nheight="+height+
+                    "\nfav cha="+fav+
+                    "\nannual income="+annualin
+                    );
+    
+
+    }
+}
+
